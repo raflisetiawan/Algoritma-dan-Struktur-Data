e2892d48cecc8ee52a14b080cb6524b61ffff932
raflisetiawan <111565262+raflisetiawan@users.noreply.github.com>
2022-11-30 16:09:40 +0700

    BinarySearchTree

---
+++ BinarySearchTree.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: UMUM
+ * Date: 30/11/2022
+ * Time: 16:02
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Halo{
+	
+public class BinarySearchTree
+	{
+		public Node Root;
+		
+		public BinarySearchTree()
+		{
+			Root = null;
+		}
+		
+		public void Add(int data){
+			// buat node baru untuk data yang akan ditambahkan
+			Node baru = new Node(data);
+			
+			// cek apakah tree masih kosong
+			if(Root == null){
+				Root = baru;
+			}else{
+				bool inserted = false;
+				Node temp = Root;
+				while(inserted == false){
+					// bandingkan data baru dengan temp
+					if(baru.Data < temp.Data){
+						// letakkan data baru sebagai left child
+						if(temp.LeftChild == null){
+							temp.LeftChild = baru;
+							baru.Parent = temp;
+							inserted = true;
+						}else{
+							temp = temp.LeftChild;
+						}
+					}else{
+						if(temp.RightChild == null){
+							temp.RightChild = baru;
+							baru.Parent = temp;
+							inserted = true;
+						}else{
+							temp = temp.RightChild;
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Node.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: UMUM
+ * Date: 30/11/2022
+ * Time: 16:03
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+ 
+namespace Halo{
+	
+
+public class Node
+	{
+	public int Data;
+	public Node Parent;
+	public Node LeftChild;
+	public Node RightChild;
+	
+	public Node(int data)
+	{
+		this.Data = data;
+	}
+}
+}
